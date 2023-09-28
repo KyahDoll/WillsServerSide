a07351d15e4a39d14cb0b299135654e025adbc6c
Kyah Dollinger <113926823+KyahDoll@users.noreply.github.com>
2023-09-28 16:31:17 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using var httpClient = new HttpClient();
+        string apiUrl = "https://swapi.dev/api/people/";
+
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"HTTP Request Error: {e.Message}");
+        }
+    }
+}
